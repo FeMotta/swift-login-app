12cf539677f4fcf00dec17489150b28825533c4f
Fernando Motta <fernandosantosmotta@gmail.com>
2022-08-13 16:01:56 -0300

    Initial Commit

---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by Fernando dos Santos Motta on 13/08/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
